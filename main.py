ecfa34344616f9c08f2d36e92a208149885d4822
Paulina Radomska <paulinaradomska10@wp.pl>
2023-05-01 11:18:22 +0200

    create graph

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import requests
 
+USER = "paulina"
+TOKEN = "jwdcnwdcjw3cw"
+
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER}/graphs"
 
 user_params = {
-    "token": "jwdcnwdcjw3cw",
-    "username": "paulina",
+    "token": TOKEN,
+    "username": USER,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
+
+graph_params = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "Pages",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
+# print(response.text)
+
