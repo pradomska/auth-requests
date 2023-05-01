8262725c4d6b7d5d1eff78456f7ba808c75147a9
Paulina Radomska <paulinaradomska10@wp.pl>
2023-05-01 11:26:32 +0200

    add first progress in my graph

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
+from datetime import datetime
 
 USER = "paulina"
 TOKEN = "jwdcnwdcjw3cw"
+GRAPH_ID = "graph1"
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER}/graphs"
+GRAPH_ID_ENDPOINT = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 
 user_params = {
     "token": TOKEN,
@@ -16,7 +19,7 @@ user_params = {
 # print(response.text)
 
 graph_params = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Reading Graph",
     "unit": "Pages",
     "type": "float",
@@ -29,3 +32,9 @@ headers = {
 # response = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
 # print(response.text)
 
+graph_id_params = {
+    "date": "20230501",
+    "quantity": "2",
+}
+response = requests.post(url=GRAPH_ID_ENDPOINT, json=graph_id_params, headers=headers)
+print(response.text)
